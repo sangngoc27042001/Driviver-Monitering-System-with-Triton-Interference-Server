132650f0b25047c9c17662976f2a024466be852d
sangngoc27042001 <69387187+sangngoc27042001@users.noreply.github.com>
2023-01-31 21:14:09 +0700

    a

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-pip install -r requirements.txt
+pip3 install torch torchvision torchaudio
+pip3 install pandas mediapipe gitpython ipython psutil PyYAML tqdm seaborn
 apt-get update && apt-get install libgl1
 
