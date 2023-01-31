93e76371397fe19512dd1ba00b8c1fa8955cde06
sangngoc27042001 <69387187+sangngoc27042001@users.noreply.github.com>
2023-01-31 14:49:50 +0700

    a

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-git clone https://github.com/sangngoc27042001/triton-interference-server.git
 pip install mediapipe
 pip install -qr https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt
 apt-get update && apt-get install libgl1
